309f67fe16efa6b6647fb068458d9b071b16e97a
Mateus Lopes <mateusfellipe2302@gmail.com>
2021-03-11 10:39:44 -0300

    Updates

--- index.js
+++ index.js
@@ -1,96 +1,98 @@
 var svg = d3.select("svg"),
-        margin = 20,
-        diameter = +svg.attr("width"),
-        g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+  margin = 20,
+  diameter = +svg.attr("width"),
+  g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-    var color = d3.scaleLinear()
-        .domain([-1, 5])
-        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-        .interpolate(d3.interpolateHcl);
+var color = d3.scaleLinear()
+  .domain([-1, 5])
+  .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+  .interpolate(d3.interpolateHcl);
 
-    var pack = d3.pack()
-        .size([diameter - margin, diameter - margin])
-        .padding(2);
+var pack = d3.pack()
+  .size([diameter - margin, diameter - margin])
+  .padding(2);
 
-    d3.json("youtube-dl.json", function(error, root) {
-      if (error) throw error;
+var file = document.getElementById("my_data_json").getAttribute("data-name")
 
-      root = d3.hierarchy(root)
-          .sum(function(d) { return d.size; })
-          .sort(function(a, b) { return b.value - a.value; });
+d3.json(file, function (error, root) {
+  if (error) throw error;
 
-      var focus = root,
-          nodes = pack(root).descendants(),
-          view;
+  root = d3.hierarchy(root)
+    .sum(function (d) { return d.size; })
+    .sort(function (a, b) { return b.value - a.value; });
 
-      var circle = g.selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-          .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
-          .style("fill", function(d) { return d.children ? color(d.depth) : d.data.color; })
-          .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+  var focus = root,
+    nodes = pack(root).descendants(),
+    view;
 
-      var text = g.selectAll("text")
-        .data(nodes)
-        .enter().append("text")
-          .attr("class", "label")
-          .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
-          .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
-          .style("font-weight", function(d){ return "bold";})
-          .style("font-size", function(d){ return "12px";})
-          .text(function(d) { return d.children == null ? 'Name: ' + d.data.name + '()' : d.data.name; })
-          .append('svg:tspan')
-          .attr('x', 0)
-          .attr('dy', 20)
-          .text(function(d) { return d.children == null ? 'Complex: '+ d.data.size  : ''; })
-          .append('svg:tspan')
-          .attr('x', 0)
-          .attr('dy', 20)
-          .text(function(d) { return d.children == null ? 'Cat: '+ d.data.category  : ''; })
-          .append('svg:a')
-          .attr('href', function(d) { return d.children == null ? d.data.url  : ''; })
-          .attr('target','_blank')
-          .attr('fill','#008CBA')
-          .append('svg:tspan')
-          .attr('x', 0)
-          .attr('dy', 20)
-          .text(function(d) { return d.children == null ? 'GitHub Location' : ''; })
-          .style("fill", '#008CBA')
-          .style('pointer-events','auto')
-          .style('text-decoration', 'underline')
-          .style("font-weight", function(d){ return "800";})
-          .style("font-size", function(d){ return "13px";});
+  var circle = g.selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+    .attr("class", function (d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+    .style("fill", function (d) { return d.children ? color(d.depth) : d.data.color; })
+    .on("click", function (d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
 
-      var node = g.selectAll("circle,text");
+  var text = g.selectAll("text")
+    .data(nodes)
+    .enter().append("text")
+    .attr("class", "label")
+    .style("fill-opacity", function (d) { return d.parent === root ? 1 : 0; })
+    .style("display", function (d) { return d.parent === root ? "inline" : "none"; })
+    .style("font-weight", function (d) { return "bold"; })
+    .style("font-size", function (d) { return "12px"; })
+    .text(function (d) { return d.children == null ? 'Name: ' + d.data.name + '()' : d.data.name; })
+    .append('svg:tspan')
+    .attr('x', 0)
+    .attr('dy', 20)
+    .text(function (d) { return d.children == null ? 'Complex: ' + d.data.size : ''; })
+    .append('svg:tspan')
+    .attr('x', 0)
+    .attr('dy', 20)
+    .text(function (d) { return d.children == null ? 'Cat: ' + d.data.category : ''; })
+    .append('svg:a')
+    .attr('href', function (d) { return d.children == null ? d.data.url : ''; })
+    .attr('target', '_blank')
+    .attr('fill', '#008CBA')
+    .append('svg:tspan')
+    .attr('x', 0)
+    .attr('dy', 20)
+    .text(function (d) { return d.children == null ? 'GitHub Location' : ''; })
+    .style("fill", '#008CBA')
+    .style('pointer-events', 'auto')
+    .style('text-decoration', 'underline')
+    .style("font-weight", function (d) { return "800"; })
+    .style("font-size", function (d) { return "13px"; });
 
-      svg
-          .style("background", "none")
-          .style("border-radius", "50px")
-          .on("click", function() { zoom(root); });
+  var node = g.selectAll("circle,text");
 
-      zoomTo([root.x, root.y, root.r * 2 + margin]);
+  svg
+    .style("background", "none")
+    .style("border-radius", "50px")
+    .on("click", function () { zoom(root); });
 
-      function zoom(d) {
-        var focus0 = focus; focus = d;
+  zoomTo([root.x, root.y, root.r * 2 + margin]);
 
-        var transition = d3.transition()
-            .duration(d3.event.altKey ? 7500 : 750)
-            .tween("zoom", function(d) {
-              var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
-              return function(t) { zoomTo(i(t)); };
-            });
+  function zoom(d) {
+    var focus0 = focus; focus = d;
 
-        transition.selectAll("text")
-          //.filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-            .style("fill-opacity", function(d) { return 1; })
-            .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-            .on("end", function(d) { if (d.parent !== focus && d.parent.children !== focus) this.style.display = "none"; });
-      }
+    var transition = d3.transition()
+      .duration(d3.event.altKey ? 7500 : 750)
+      .tween("zoom", function (d) {
+        var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+        return function (t) { zoomTo(i(t)); };
+      });
 
-      function zoomTo(v) {
-        var k = diameter / v[2]; view = v;
-        node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-        circle.attr("r", function(d) { return d.r * k; });
-      }
-    });
+    transition.selectAll("text")
+      //.filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+      .style("fill-opacity", function (d) { return 1; })
+      .on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
+      .on("end", function (d) { if (d.parent !== focus && d.parent.children !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function (d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function (d) { return d.r * k; });
+  }
+});
 
