83f2d0d92bae84f44ee2bac388e7dcb7c96ffd30
Mateus Lopes <mateusfellipe2302@gmail.com>
2021-03-10 23:10:52 -0300

    Updates

--- index.js
+++ index.js
@@ -85,9 +85,9 @@ d3.json(file, function (error, root) {
 
         transition.selectAll("text")
             .filter(function (d) { return d.parent === focus || this.style.display === "inline"; })
-            .style("fill-opacity", function (d) { return d.parent === focus ? 1 : 0; })
-            .on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
-            .on("end", function (d) { if (d.parent !== focus) this.style.display = "none"; });
+            .style("fill-opacity", function (d) { return d.parent === focus ? 1 : 0; });
+            //.on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
+            //.on("end", function (d) { if (d.parent !== focus) this.style.display = "none"; });
     }
 
     function zoomTo(v) {
